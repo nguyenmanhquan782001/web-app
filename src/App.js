51ea98c1ff95f6016ad1f21ce18fe2daff9e5a7a
Manh Quan <manhquana5yl@gmail.com>
2021-03-18 08:50:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Albums from './components/Albums';
+import Banner from './components/Banner.js';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+
+        <Header />
+
+        <main>
+          <Banner />
+          <Albums />
+        </main>
+        <Footer />
+      </div>
+
     </div>
   );
 }
